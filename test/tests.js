a311fab4a8e6be0d689ac376838c0bfa55617d85
Jeremy <jeremy.worden@gmail.com>
2023-10-09 14:22:57 -0700

    v1.2.2 - Tested against CUCM 15.x

--- test/tests.js
+++ test/tests.js
@@ -1,14 +1,35 @@
 const perfMonService = require("../main");
+const path = require('path');
+const { cleanEnv, str, host } = require("envalid");
 
-// Set up new PerfMon service
-let service = new perfMonService("10.10.20.1", "administrator", "ciscopsdt");
+// If not production load the local env file
+if(process.env.NODE_ENV === "development"){
+  require('dotenv').config({ path: path.join(__dirname, '..', 'env', 'development.env') })
+}else if(process.env.NODE_ENV === "test"){
+  require('dotenv').config({ path: path.join(__dirname, '..', 'env', 'test.env') })
+}else if(process.env.NODE_ENV === "staging"){
+  require('dotenv').config({ path: path.join(__dirname, '..', 'env', 'staging.env') })
+}
 
-var host = "hq-cucm-pub";
+const env = cleanEnv(process.env, {
+  NODE_ENV: str({
+    choices: ["development", "test", "production", "staging"],
+    desc: "Node environment",
+  }),
+  CUCM_HOSTNAME: host({ desc: "Cisco CUCM Hostname or IP Address to send the perfmon request to. Typically the publisher." }),
+  CUCM_USERNAME: str({ desc: "Cisco CUCM AXL Username." }),
+  CUCM_PASSWORD: str({ desc: "Cisco CUCM AXL Password." }),
+  CUCM_SERVER_NAME: str({ desc: "The CUCM name or IP address of the target server from which the client wants to retrieve the counter information from. Note this could be any node in the cluster." , example: "hq-cucm-pub or hq-cucm-sub" }),
+});
+
+let service = new perfMonService(env.CUCM_HOSTNAME, env.CUCM_USERNAME, env.CUCM_PASSWORD);
+
+var cucmServerName = env.CUCM_SERVER_NAME;
 
 // Variables to hold our SessionID and our Session Counter
 var SessionID;
 var counterObj = {
-  host: host,
+  host: cucmServerName,
   object: "Processor(_Total)",
   counter: "% CPU Time",
 };
@@ -75,7 +96,7 @@ var counterObj = {
 
   console.log("Let's collect some non session counter data.");
   await service
-    .collectCounterData(host, "Cisco Hunt Pilots")
+    .collectCounterData(cucmServerName, "Cisco CallManager")
     .then((results) => {
       console.log("collectCounterData", results);
     })
@@ -87,7 +108,7 @@ var counterObj = {
     "Let's returns the list of available PerfMon objects and counters on a particular host"
   );
   await service
-    .listCounter(host)
+    .listCounter(cucmServerName)
     .then((results) => {
       console.log("listCounter", results);
     })
@@ -99,7 +120,7 @@ var counterObj = {
     "Let's return a list of instances of a PerfMon object on a particular host. Instances of an object can dynamically change. This operation returns the most recent list."
   );
   await service
-    .listInstance(host, "Processor")
+    .listInstance(cucmServerName, "Processor")
     .then((results) => {
       console.log("listInstance", results);
     })
